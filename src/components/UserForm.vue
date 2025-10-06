d654b38a69e8ac1dee64ac7040c9715215a3818c
Scott Yuan <scoworthyuan@gmail.com>
2025-10-07 09:54:03 +1100

    added suburb input box

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,128 +1,134 @@
 <template>
   <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-8 offset-md-2">
-        <h1 class="text-center">User Information Form</h1>
+    <div class="col-md-8 offset-md-2">
+      <h1 class="text-center">User Information Form</h1>
 
-        <form @submit.prevent="submitForm">
-          <!-- Username / Password -->
-          <div class="row mb-3">
-            <div class="col-sm-6 col-md-6">
-              <label for="username" class="form-label">Username</label>
-              <input
-                type="text"
-                class="form-control"
-                id="username"
-                @blur="() => validateName(true)"
-                @input="() => validateName(false)"
-                v-model="formData.username"
-              />
-              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
-            </div>
-
-            <div class="col-sm-6 col-md-6">
-              <label for="gender" class="form-label">Gender</label>
-              <select
-                class="form-select"
-                id="gender"
-                v-model="formData.gender"
-                @blur="() => validateGender(true)"
-                @change="() => validateGender(false)"
-              >
-                <option disabled value="">Select…</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-              <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
-            </div>
-
-            <!-- Password -->
-            <div class="row mb-3">
-              <div class="col-md-6 col-sm-6">
-                <label for="password" class="form-label">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="password"
-                  @blur="() => validatePassword(true)"
-                  @input="() => validatePassword(false)"
-                  v-model="formData.password"
-                />
-                <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
-              </div>
-
-              <div class="col-md-6 col-sm-6">
-                <label for="confirm-password" class="form-label">Confirm password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="confirm-password"
-                  v-model="formData.confirmPassword"
-                  @blur="() => validateConfirmPassword(true)"
-                  @input="() => validateConfirmPassword(false)"
-                />
-                <div v-if="errors.confirmPassword" class="text-danger">
-                  {{ errors.confirmPassword }}
-                </div>
-              </div>
-            </div>
+      <form @submit.prevent="submitForm">
+        <!-- Username / Password -->
+        <div class="row mb-3">
+          <div class="col-sm-6 col-md-6">
+            <label for="username" class="form-label">Username</label>
+            <input
+              type="text"
+              class="form-control"
+              id="username"
+              @blur="() => validateName(true)"
+              @input="() => validateName(false)"
+              v-model="formData.username"
+            />
+            <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
           </div>
 
-          <!-- Checkbox -->
+          <div class="col-sm-6 col-md-6">
+            <label for="gender" class="form-label">Gender</label>
+            <select
+              class="form-select"
+              id="gender"
+              v-model="formData.gender"
+              @blur="() => validateGender(true)"
+              @change="() => validateGender(false)"
+            >
+              <option disabled value="">Select…</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
+          </div>
+
+          <!-- Password -->
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
-              <div class="form-check">
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  id="isAustralian"
-                  @blur="() => validateResident(true)"
-                  @change="() => validateResident(false)"
-                  v-model="formData.isAustralian"
-                />
-                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
-                <div v-if="errors.resident" class="text-danger">{{ errors.resident }}</div>
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+            </div>
+
+            <div class="col-md-6 col-sm-6">
+              <label for="confirm-password" class="form-label">Confirm password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="confirm-password"
+                v-model="formData.confirmPassword"
+                @blur="() => validateConfirmPassword(true)"
+                @input="() => validateConfirmPassword(false)"
+              />
+              <div v-if="errors.confirmPassword" class="text-danger">
+                {{ errors.confirmPassword }}
               </div>
             </div>
           </div>
+        </div>
 
-          <!-- Reason -->
-          <div class="mb-3">
-            <label for="reason" class="form-label">Reason for joining</label>
-            <textarea
-              class="form-control"
-              id="reason"
-              rows="3"
-              v-model="formData.reason"
-              @blur="() => validateReason(true)"
-              @input="() => validateReason(false)"
-            ></textarea>
-            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
-            <div v-if="friendMessage" class="text-success">{{ friendMessage }}</div>
+        <!-- Checkbox -->
+        <div class="row mb-3">
+          <div class="col-md-6 col-sm-6">
+            <div class="form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="isAustralian"
+                @blur="() => validateResident(true)"
+                @change="() => validateResident(false)"
+                v-model="formData.isAustralian"
+              />
+              <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+              <div v-if="errors.resident" class="text-danger">{{ errors.resident }}</div>
+            </div>
           </div>
+        </div>
 
-          <!-- Actions -->
+        <!-- Reason -->
+        <div class="row mb-3">
+          <label for="reason" class="form-label">Reason for joining</label>
+          <textarea
+            class="form-control"
+            id="reason"
+            rows="3"
+            v-model="formData.reason"
+            @blur="() => validateReason(true)"
+            @input="() => validateReason(false)"
+          ></textarea>
+          <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
+          <div v-if="friendMessage" class="text-success">{{ friendMessage }}</div>
+        </div>
+
+        <!-- Actions -->
+        <div class="row mb-3">
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
             <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
           </div>
-        </form>
-      </div>
-    </div>
-  </div>
+        </div>
 
-  <!-- Display submitted items in a PrimeVue DataTable -->
-  <div class="row mt-5" v-if="submittedCards.length">
-    <DataTable :value="submittedCards" dataKey="username" responsiveLayout="scroll">
-      <Column field="username" header="Username" />
-      <Column field="password" header="Password" />
-      <Column header="Australia Resident">
-        <template #body="{ data }">{{ data.isAustralian ? 'Yes' : 'No' }}</template>
-      </Column>
-      <Column field="gender" header="Gender" />
-      <Column field="reason" header="Reason" />
-    </DataTable>
+        <!-- Suburb -->
+        <div class="mb-3">
+          <label for="reason" class="form-label">Suburb</label>
+          <input type="text" class="form-control" id="suburb" v-bind:value="formData.suburb" />
+        </div>
+      </form>
+    </div>
+
+    <!-- Display submitted items in a PrimeVue DataTable -->
+    <div class="row mt-5" v-if="submittedCards.length">
+      <DataTable :value="submittedCards" dataKey="username" responsiveLayout="scroll">
+        <Column field="username" header="Username" />
+        <Column field="password" header="Password" />
+        <Column header="Australia Resident">
+          <template #body="{ data }">{{ data.isAustralian ? 'Yes' : 'No' }}</template>
+        </Column>
+        <Column field="gender" header="Gender" />
+        <Column field="reason" header="Reason" />
+      </DataTable>
+    </div>
   </div>
 </template>
 
@@ -138,6 +144,7 @@ const formData = ref({
   isAustralian: false,
   reason: '',
   gender: '',
+  suburb: 'Clayton',
 })
 
 const submittedCards = ref([])
