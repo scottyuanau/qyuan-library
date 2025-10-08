2ede1504beeec48b30edbd4d481a0c30b50bc786
Scott Yuan <scoworthyuan@gmail.com>
2025-10-08 16:38:21 +1100

    added firebase function to update book counts

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /**
  * Import function triggers from their respective submodules:
  *
@@ -8,9 +9,26 @@
  */
 
 const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const admin = require("firebase-admin")
+const cors = require("cors")({origin: true});
 
+admin.initializeApp()
+
+exports.countBooks = onRequest((req, res) => {
+  cors(req, res, async() => {
+    try {
+      const bookCollection = admin.firestore().collection("books");
+      const snapshot = await booksCollection.get()
+      const count = snapshot.size()
+
+      res.status(200).send({count})
+    } catch (error) {
+      console.error("Error counting books: ", error.message)
+      res.status(500).send("Error counting books")
+    }
+  })
+})
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
 // traffic spikes by instead downgrading performance. This limit is a
